01a3ff68ed31aaa6c476b0c3c3c6f6bc52f4d47d
Nam <namvu1291@gmail.com>
2019-12-24 23:54:32 -0600

    added memory usage to metrics endpoint

--- src/utils/prometheus.h
+++ src/utils/prometheus.h
@@ -12,9 +12,12 @@ public:
   std::string to_string();
   std::vector<prometheus::MetricFamily> Collect() const override;
   void update_stats();
+  void update_mem_usage();
 
 private:
   prometheus::Gauge& m_fds;
+  prometheus::Gauge& m_vmrss;
+  prometheus::Gauge& m_vmsize;
 };
 
 }  // namespace restor
