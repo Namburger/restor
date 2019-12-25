4eb85fabf3509f4fb47b41d90ddf3bd81c0901a7
Nam <namvu1291@gmail.com>
2019-12-24 23:54:32 -0600

    added memory usage to metrics endpoint

--- src/utils/prometheus.cc
+++ src/utils/prometheus.cc
@@ -3,16 +3,25 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#include <fstream>
+#include <iostream>
 #include <limits>
+#include <regex>
 #include <string>
 #include <vector>
 
+#include "boost/regex.hpp"
 #include "prometheus/gauge.h"
 #include "prometheus/registry.h"
 #include "prometheus/text_serializer.h"
 
+using std::ifstream;
 using std::numeric_limits;
+using std::regex;
+using std::regex_replace;
+using std::stoul;
 using std::string;
+using std::stringstream;
 using std::vector;
 
 namespace restor {
@@ -22,7 +31,17 @@ Registry::Registry()
                 .Name("process_open_fds")
                 .Help("Number of open file descriptors")
                 .Register(*this)
-                .Add({})) {}
+                .Add({})),
+      m_vmrss(prometheus::BuildGauge()
+                  .Name("process_resident_memory_bytes")
+                  .Help("Process resident memory size in bytes.")
+                  .Register(*this)
+                  .Add({})),
+      m_vmsize(prometheus::BuildGauge()
+                   .Name("process_virtual_memory_bytes")
+                   .Help("Process virtual memory size in bytes.")
+                   .Register(*this)
+                   .Add({})) {}
 
 Registry& Registry::get_registry() {
   static Registry r;
@@ -31,6 +50,24 @@ Registry& Registry::get_registry() {
 
 vector<prometheus::MetricFamily> Registry::Collect() const {
   return prometheus::Registry::Collect();
+}
+
+void Registry::update_mem_usage() {
+  // interested in VmRSS and VmSize
+  ifstream mem_status{"/proc/self/status"};
+  string line;
+  while (mem_status.good()) {
+    getline(mem_status, line);
+    auto sub5 = line.substr(0, 5);
+    auto sub6 = line.substr(0, 6);
+    if (sub5 == "VmRSS") {
+      string num = regex_replace(line, regex(R"([\D])"), "");
+      m_vmrss.Set(stoul(num.c_str()) * 1024);
+    } else if (sub6 == "VmSize") {
+      string num = regex_replace(line, regex(R"([\D])"), "");
+      m_vmsize.Set(stoul(num.c_str()) * 1024);
+    }
+  }
 }
 
 void Registry::update_stats() {
@@ -44,6 +81,7 @@ void Registry::update_stats() {
     }
   }
   m_fds.Set(fd_counter);
+  update_mem_usage();
 }
 
 string Registry::to_string() {
