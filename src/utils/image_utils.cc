cf2cd44383434b201eb635a20108ea595c0bb661
Nam <namvu1291@gmail.com>
2019-12-26 19:23:35 -0600

    make sure server doesn't break after receiving gargabe data and added /info

--- src/utils/image_utils.cc
+++ src/utils/image_utils.cc
@@ -86,48 +86,13 @@ std::vector<uint8_t> DecodeBmp(
           output[dst_pos + 3] = input[src_pos + 3];
           break;
         default:
-          LOG(FATAL) << "Unexpected number of channels: " << channels;
+          LOG(INFO) << "Unexpected number of channels: " << channels;
+          return {};
           break;
       }
     }
   }
   return output;
-}
-
-// Reads BMP image. It will crahs upon failure.
-std::vector<uint8_t> ReadBmp(const std::string& input_bmp_name, ImageDims* image_dims) {
-  std::string file_content;
-  ReadFileOrDie(input_bmp_name, &file_content);
-  CHECK(!file_content.empty()) << "Bmp image file is empty " << input_bmp_name;
-  const uint8_t* img_bytes = reinterpret_cast<const uint8_t*>(file_content.data());
-
-  // Data in BMP file header is stored in Little Endian format. The following
-  // method should work on both Big and Little Endian machine.
-  auto to_int32 = [](const unsigned char* p) -> int32_t {
-    return p[0] | (p[1] << 8) | (p[2] << 16) | (p[3] << 24);
-  };
-  const int32_t header_size = to_int32(img_bytes + 10);
-  const int32_t bpp = to_int32(img_bytes + 28);
-  int* width = image_dims->data() + 1;
-  int* height = image_dims->data();
-  int* channels = image_dims->data() + 2;
-  *width = to_int32(img_bytes + 18);
-  *height = to_int32(img_bytes + 22);
-  *channels = bpp / 8;
-  // Currently supports RGB and grayscale image at this function.
-  CHECK((*width) > 0 && (*height) > 0 && ((*channels) == 3 || (*channels) == 1))
-      << "Unsupported image format. width, height, channels: " << *width << ", " << *height << ", "
-      << *channels << "\n";
-
-  // there may be padding bytes when the width is not a multiple of 4 bytes
-  // 8 * channels == bits per pixel
-  const int row_size = (8 * (*channels) * (*width) + 31) / 32 * 4;
-  // if height is negative, data layout is top down
-  // otherwise, it's bottom up
-  bool top_down = (*height < 0);
-  // Decode image, allocating tensor once the image size is known
-  const uint8_t* bmp_pixels = &img_bytes[header_size];
-  return DecodeBmp(bmp_pixels, row_size, *width, abs(*height), *channels, top_down);
 }
 
 // Resizes BMP image.
@@ -182,7 +147,9 @@ void ResizeImage(
 
 // Converts RGB image to grayscale. Take the average.
 std::vector<uint8_t> RgbToGrayscale(const std::vector<uint8_t>& in, const ImageDims& in_dims) {
-  CHECK_GE(in_dims[2], 3);
+  if (in_dims[2] < 3) {
+    return {};
+  }
   std::vector<uint8_t> result;
   int out_size = in_dims[0] * in_dims[1];
   result.resize(out_size);
@@ -213,9 +180,11 @@ std::vector<uint8_t> ReadBmpData(const std::string& input_bmp_data, ImageDims* i
   *height = to_int32(img_bytes + 22);
   *channels = bpp / 8;
   // Currently supports RGB and grayscale image at this function.
-  CHECK((*width) > 0 && (*height) > 0 && ((*channels) == 3 || (*channels) == 1))
-      << "Unsupported image format. width, height, channels: " << *width << ", " << *height << ", "
-      << *channels << "\n";
+  if (!((*width) > 0 && (*height) > 0 && ((*channels) == 3 || (*channels) == 1))) {
+    LOG(INFO) << "Unsupported image format. width, height, channels: " << *width << ", " << *height
+              << "," << *channels << "\n";
+    return {};
+  }
 
   // there may be padding bytes when the width is not a multiple of 4 bytes
   // 8 * channels == bits per pixel
@@ -230,25 +199,6 @@ std::vector<uint8_t> ReadBmpData(const std::string& input_bmp_data, ImageDims* i
 
 }  // namespace
 
-std::vector<uint8_t> GetInputFromImage(
-    const std::string& image_path, const ImageDims& target_dims) {
-  std::vector<uint8_t> result;
-  if (!EndsWith(image_path, ".bmp")) {
-    LOG(FATAL) << "Unsupported image type: " << image_path;
-    return result;
-  }
-  result.resize(ImageDimsToSize(target_dims));
-  ImageDims image_dims;
-  std::vector<uint8_t> in = ReadBmp(image_path, &image_dims);
-  CHECK(!in.empty()) << "Fail to read bmp image from file: " << image_path;
-
-  if (target_dims[2] == 1 && (image_dims[2] == 3 || image_dims[2] == 4)) {
-    in = RgbToGrayscale(in, image_dims);
-  }
-  ResizeImage(image_dims, in.data(), target_dims, result.data());
-  return result;
-}
-
 namespace addon {
 
 std::vector<uint8_t> GetInputFromData(
@@ -257,11 +207,11 @@ std::vector<uint8_t> GetInputFromData(
   result.resize(ImageDimsToSize(target_dims));
   ImageDims image_dims;
   std::vector<uint8_t> in = ReadBmpData(base64_decode(base64_bmp_data), &image_dims);
-  CHECK(!in.empty()) << "Fail to read bmp image from data";
-
+  if (in.empty()) return {};
   if (target_dims[2] == 1 && (image_dims[2] == 3 || image_dims[2] == 4)) {
     in = RgbToGrayscale(in, image_dims);
   }
+  if (in.empty()) return {};
   ResizeImage(image_dims, in.data(), target_dims, result.data());
   return result;
 }
