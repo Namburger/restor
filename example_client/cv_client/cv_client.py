2199c958d9808edb45ef51afeccb32d7569c9f86
Nam <namvu1291@gmail.com>
2020-02-05 18:24:14 -0600

    started, never finished

--- example_client/cv_client/cv_client.py
+++ example_client/cv_client/cv_client.py
@@ -5,59 +5,57 @@ import base64
 import argparse
 
 parser = argparse.ArgumentParser(
-    description='Taking an image and send it to restor for processing.')
+    description='Send video frames to restor for processing.')
 parser.add_argument('--host', type=str, default='localhost',
                     help="ip of the restor server.")
 parser.add_argument('--port', type=str, default='8888',
                     help='port of the restor server.')
-
-# Take an image
-def take_image(file):
-    camera_port = 0
-    camera = cv2.VideoCapture(camera_port)
-
-    def get_image():
-        retval, im = camera.read()
-        return im
-
-    # will take 10 tmp shots to warm up the camera
-    for i in range(10):
-        temp = get_image()
-    print('Taking image...')
-    captured = get_image()
-    print('Writing image to captured.bmp')
-    cv2.imwrite(file, captured)
-    del(camera)
 
 # send the post request
 def send_request(URL, payload):
     print('Sending @data={\"data\": base64_encode(captured.bmp)} to', URL)
     resp = requests.post(url=URL, data=payload, headers={
         'content-type': 'application/json'})
-    j = json.loads(resp.text)
-    print(json.dumps(j, indent=2, sort_keys=True))
-
+    return json.loads(resp.text)
 
 def main():
     args = parser.parse_args()
     print(args)
     URL = 'http://' + args.host + ':' + args.port + '/detects'
-
-    file = '/tmp/captured.bmp'
-    take_image(file)
-
-    # get payload
-    with open(file, 'rb') as image:
-        data = base64.b64encode(image.read()).decode('utf-8')
-        payload = json.dumps({'data': data})
-    send_request(URL, payload)
-
-    # show image
-    img = cv2.imread(file)
-    cv2.imshow("captured", img)
-    cv2.waitKey(0)
-    cv.destroyAllWindows()
-
+    tmp_file = '/tmp/captured.bmp'
+    frame = cv2.VideoCapture(0)
+    frame.set(cv2.CAP_PROP_FRAME_WIDTH, 300)
+    frame.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
+    frame.set(cv2.CAP_PROP_FPS, 30)
+    frame_num = 0
+    while True:
+        print('-----------------------------------')
+        print('FRAME NUM:', frame_num)
+        ret, img = frame.read()
+        cv2.imwrite(tmp_file, img)
+        # get payload
+        with open(tmp_file, 'rb') as image:
+            data = base64.b64encode(image.read()).decode('utf-8')
+            payload = json.dumps({'data': data})
+        j = send_request(URL, payload)
+        for i in range(1,4):
+            key = 'result' + str(i)
+            candidate = j[key]['candidate']
+            score = j[key]['score']
+            start = (int(j[key]['xmin']*300), int(j[key]['ymin']*300))
+            end = (int(j[key]['xmax']*300), int(j[key]['ymax']*300))
+            print("candidate", candidate)
+            print("score", score)
+            print("start", start)
+            print("end", end)
+            img = cv2.rectangle(img, start, end, (255, 0, 0), 2)
+        cv2.imshow('Restor CV Client Demo', img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        frame_num += 1
+        print('\n')
+    frame.release()
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
